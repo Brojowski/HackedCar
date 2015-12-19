84f69964ec254dacf8fdf9eff17c9d1950ec7a45
Brojowski <alex.gajowski@gmail.com>
2015-12-19 04:38:18 -0500

    executing unix commands i

--- routes/gpio.js
+++ routes/gpio.js
@@ -10,7 +10,6 @@ var sys = require("sys");
 var exec = require("child_process").exec;
 function puts(error, stdout, stderr){ sys.puts(stdout);}
 exec("gpio -g 2 1",puts);
-exec("gpio -g 2 0",puts);
 
 //open required pins
 //pins.open() --ect
